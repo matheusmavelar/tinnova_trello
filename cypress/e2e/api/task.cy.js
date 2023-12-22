21b28bf10467664a48fc73a08b191a5f79c18745
matheusmavelar <65365999+matheusmavelar@users.noreply.github.com>
2023-12-22 19:54:00 -0300

    Iniciando refatoracao

--- cypress/e2e/api/task.cy.js
+++ cypress/e2e/api/task.cy.js
@@ -1,44 +1,33 @@
 describe('Teste', () => {
 
-    it('criarBoard', () => {
-        cy.request({
-            method: 'POST',
-            url: 'https://api.trello.com/1/cards?idList=6585f75482efa3b16d07ec98&key=5d41b3b238b87cb8bef98e5c18c8fb76&token=ATTAedca4181a52a246dbed8eaebbd3db6102a56ba495b34e89aa482533ad828a044A940F839',
-            header: 'Accept: application/json'
-        }).then(response => {
-            cy.log(response)
-        })
+    it.skip('criarBoard', () => {
+        cy.postBoard('/1/boards/?name=', 'teste trello', '5d41b3b238b87cb8bef98e5c18c8fb76', 'ATTAedca4181a52a246dbed8eaebbd3db6102a56ba495b34e89aa482533ad828a044A940F839')
+            .then(response => {
+                cy.log(response)
+            })
     })
 
     it('buscarIdListaBoard', () => {
-        cy.request({
-            method: 'GET',
-            url: 'https://api.trello.com/1/boards/6585fa1049bfbfa5f5e955de/lists?key=5d41b3b238b87cb8bef98e5c18c8fb76&token=ATTAedca4181a52a246dbed8eaebbd3db6102a56ba495b34e89aa482533ad828a044A940F839',
-            header: 'Accept: application/json'
-        }).then(response => {
+        cy.getBoard('/1/boards/', '65860ee48320e39f8c1512ab', '/lists?','5d41b3b238b87cb8bef98e5c18c8fb76', 'ATTAedca4181a52a246dbed8eaebbd3db6102a56ba495b34e89aa482533ad828a044A940F839')
+        .then(response => {
             cy.log(response)
         })
     })
 
-    it('criarCardBoard', () => {
-        cy.request({
-            method: 'POST',
-            url: 'https://api.trello.com/1/cards?idList=6585fa1049bfbfa5f5e955e6&key=5d41b3b238b87cb8bef98e5c18c8fb76&token=ATTAedca4181a52a246dbed8eaebbd3db6102a56ba495b34e89aa482533ad828a044A940F839',
-            header: 'Accept: application/json'
-            //"6585f4c194167730fdb5a4cd"
-        }).then(response => {
-            cy.log(response)
-        })
+    it.skip('criarCardBoard', () => {
+        cy.postCardBoard('/1/cards/?', '65860ee48320e39f8c1512b2', '5d41b3b238b87cb8bef98e5c18c8fb76', 'ATTAedca4181a52a246dbed8eaebbd3db6102a56ba495b34e89aa482533ad828a044A940F839')
+            .then(response => {
+                cy.log(response)
+            })
+    
     })
 
-    it.only('deletarBoard', () => {
-        cy.request({
-            method: 'DELETE',
-            url: 'https://api.trello.com/1/cards/6585ff490ec9cfc8a9a49178?key=5d41b3b238b87cb8bef98e5c18c8fb76&token=ATTAedca4181a52a246dbed8eaebbd3db6102a56ba495b34e89aa482533ad828a044A940F839'
-        }).then(response => {
+    it.only('deletarCard', () => {
+        cy.deleteCard('/1/cards/', '658611c29e36f647a05066ae', '5d41b3b238b87cb8bef98e5c18c8fb76', 'ATTAedca4181a52a246dbed8eaebbd3db6102a56ba495b34e89aa482533ad828a044A940F839')
+        .then(response => {
             cy.log(response)
         })
-    })
+     })
 })
 
 
--- cypress/support/commandsApi.js
+++ cypress/support/commandsApi.js
@@ -2,8 +2,37 @@
 Cypress.Commands.add('getConta', (endPoint,idBoard,key,token) =>{
     cy.request({
         method: 'GET',
+        url: Cypress.env('api_trello') + endPoint +idBoard,
+        qs:{ key: key, token: token},
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+})
+
+Cypress.Commands.add('postBoard', (endPoint,name,key,token) =>{
+    cy.request({
+        method: 'POST',
         url: Cypress.env('api_trello') + endPoint,
-        qs:{idBoard: idBoard, key: key, token: token},
+        qs:{name: name, key: key, token: token},
+
+    })
+})
+
+Cypress.Commands.add('deleteBoard', (endPoint,id,key,token) =>{
+    cy.request({
+        method: 'DELETE',
+        url: Cypress.env('api_trello') + endPoint +id,
+        qs:{ key: key, token: token},
+
+    })
+})
+
+Cypress.Commands.add('getBoard', (endPoint,idBoard,endpoint2,key,token) =>{
+    cy.request({
+        method: 'GET',
+        url: Cypress.env('api_trello') + endPoint +idBoard +endpoint2,
+        qs:{ key: key, token: token},
         headers: {
             'Accept': 'application/json'
         }
@@ -11,11 +40,20 @@ Cypress.Commands.add('getConta', (endPoint,idBoard,key,token) =>{
 })
 
 
-Cypress.Commands.add('postBoard', (endPoint,name,key,token) =>{
+Cypress.Commands.add('postCardBoard', (endPoint,idList,key,token) =>{
     cy.request({
         method: 'POST',
         url: Cypress.env('api_trello') + endPoint,
-        qs:{name: name, key: key, token: token},
+        qs:{idList: idList, key: key, token: token},
+
+    })
+})
+
+Cypress.Commands.add('deleteCard', (endPoint,id,key,token) =>{
+    cy.request({
+        method: 'DELETE',
+        url: Cypress.env('api_trello') + endPoint +id,
+        qs:{ key: key, token: token},
 
     })
 })
